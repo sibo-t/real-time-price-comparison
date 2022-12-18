93fd19e42ff7cf684bc49789e8fc4549e6874531
Luyanda Lukhele <luylukhe021@student.wethinkcode.co.za>
2022-12-18 16:03:02 +0200

    fixed up api end points

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # real-time-price-comparison
+
--- src/main/java/controllers/ItemsController.java
+++ src/main/java/controllers/ItemsController.java
@@ -4,18 +4,37 @@ import io.javalin.http.Context;
 
 import models.Items;
 
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
+
 public class ItemsController {
     private static Items items;
 
-    public static void processItem(Context ctx){
-        Items item = ctx.bodyAsClass(Items.class);
-        String price = item.getPrice();
-        String description = item.getDescription();
+    //Mock Store data
+    final static List<String> store = List.of(
+            "Sasko brown bread R20",
+            "Vanilla cake R100",
+            "Golden Apples R50"
+    );
 
-        ctx.json( items = new Items(description,price));
+    public static void processItem(Context ctx){
+
+        String itemToSearchFor = ctx.body();
+
+        //TODO: @Kwanele Do your magic bellow
+
+        String found = store.stream().filter( i -> i.contains(itemToSearchFor))
+                .findFirst().orElse(null);
+        if (found != null){
+            String description = found.split("R")[0];
+            String price = found.split("R")[1];
+
+            ctx.json( items = new Items(description,price));
+        }
     }
 
     public static void viewItem(Context ctx){
-        ctx.result(items.toString());
+        ctx.json(Objects.requireNonNullElseGet(items, () -> new Items("Empty", "None")));
     }
 }
