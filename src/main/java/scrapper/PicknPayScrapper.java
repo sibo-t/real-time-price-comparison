c8418600eef3f2d9bb91148862d82d209369d886
sibo-t <101489952+sibo-t@users.noreply.github.com>
2023-01-03 14:05:40 +0200

    Add files via upload
    description as string and price as double

--- src/main/java/scrapper/PicknPayScrapper.java
+++ src/main/java/scrapper/PicknPayScrapper.java
@@ -12,22 +12,17 @@ import java.util.concurrent.TimeUnit;
 
 public class PicknPayScrapper {
 
-    public static HashMap<String, String> searchItems(String theItem) throws InterruptedException {
+    public static HashMap<String, Double> searchItems(String theItem) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        HashMap<String, String> Items = new HashMap<>();
+        HashMap<String, Double> Items = new HashMap<>();
         try {
             // Navigate to Url
-            driver.get("https://www.pnp.co.za/");
+            driver.get("https://www.pnp.co.za/pnpstorefront/pnp/en/search/?text="+theItem);
 
             TimeUnit.SECONDS.sleep(5);
 
             // Enter text "q" and perform keyboard action "Enter"
             driver.findElement(By.name("policiesCloseButton")).click();
-
-            TimeUnit.SECONDS.sleep(5);
-
-            // Find the search bar, enter search item and press ENTER
-            driver.findElement(By.id("js-site-search-input")).sendKeys(theItem + Keys.ENTER);
 
             TimeUnit.SECONDS.sleep(5);
 
@@ -37,12 +32,27 @@ public class PicknPayScrapper {
 
             // description and prices are assumed to have the same size
             for (int i = 0; i< description.size(); i++){
-                Items.put(description.get(i).getText(),prices.get(i).getText());
+                Items.put(description.get(i).getText(),convertToDouble(prices.get(i).getText()));
             }
 
         } finally {
             driver.quit();
         }
         return Items;
+    }
+
+    private static double convertToDouble(String price){
+        StringBuilder rand = new StringBuilder();
+        StringBuilder cent = new StringBuilder("00.");
+
+        for (int i = 1; i<price.length();i++){
+            if(i<price.length()-2){
+                rand.append(price.charAt(i));
+            }
+            else{
+                cent.append(price.charAt(i));
+            }
+        }
+        return Double.parseDouble(rand.toString())+Double.parseDouble(cent.toString());
     }
 }
