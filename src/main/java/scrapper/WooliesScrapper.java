7dde1cb100332947530cc39fc020840e54243758
Kwanele Myeza <114282533+KwaneleMyeza@users.noreply.github.com>
2023-01-09 20:47:42 +0200

    Add files via upload

---
+++ src/main/java/scrapper/WooliesScrapper.java
@@ -0,0 +1,69 @@
+package scrapper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WooliesScrapper {
+
+    public static HashMap<String, Double> searchItems(String theItem) throws InterruptedException {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        HashMap<String, Double> Items = new HashMap<>();
+
+        try {
+            // Navigate to Url
+            driver.get("https://www.woolworths.co.za/cat?Ntt=" + theItem);
+
+            TimeUnit.SECONDS.sleep(5);
+
+            // Enter text "q" and perform keyboard action "Enter"
+            driver.findElement(By.className("cookie-banner-accept")).click();
+
+            TimeUnit.SECONDS.sleep(5);
+
+            List<WebElement> description = driver.findElements(By.className("range--title"));
+
+            List<WebElement> prices = driver.findElements(By.className("product-card__actions"));
+
+//             description and prices are assumed to have the same size
+            for (int i = 0; i< description.size(); i++){
+
+                if (!Items.containsKey((description.get(i).getText()))) {
+                    Items.put(description.get(i).getText(), convertToDouble(prices.get(i).getText()));
+                }
+            }
+
+        } finally {
+            driver.quit();
+        }
+        return Items;
+    }
+
+    private static double convertToDouble(String price){
+        StringBuilder rand = new StringBuilder();
+        StringBuilder cent = new StringBuilder("00.");
+
+        for (int i = 1; i<price.length();i++){
+            if(i<price.length()-2){
+                rand.append(price.charAt(i));
+            }
+            else{
+                cent.append(price.charAt(i));
+            }
+        }
+        return Double.parseDouble(rand.toString())+Double.parseDouble(cent.toString());
+    }
+
+}
