6ef5702801972783167d13f30bf2bfaea0a6bc42
sibo-t <stmsabala@gmail.com>
2023-01-29 11:43:20 +0200

    update shoprite

--- src/main/java/scrapper/ShopriteScrapper.java
+++ src/main/java/scrapper/ShopriteScrapper.java
@@ -1,5 +1,7 @@
 package scrapper;
 
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,21 +23,19 @@ public class ShopriteScrapper extends StoreScrapper{
             driver.get("https://www.shoprite.co.za/search/all?q="+theItem);
 
            //Retrieve the descriptions and prices of searched items
-            List<WebElement> description = driver.findElements(
-                    By.className("item-product__details")
+            List<WebElement> content = driver.findElements(
+                    By.className("row")
             );
 
-            List<WebElement> prices = driver.findElements(By.className("special-price__price"));
-
             // description and prices should have the same size
-            for (int i = 0; i< description.size(); i++){
-                String price = prices.get(i).getText();
+            for (int i = 0; i< content.size(); i++){
+                String attributes = content.get(content.size()-1).getAttribute("data-product-ga");
+                JSONArray attributeArray = new JSONArray(attributes);
+                JSONObject productJson = attributeArray.getJSONObject(i);
 
-                //Avoid empty string
-                if(price.equals("")){
-                    price = "R0,00";
-                }
-                Items.put(description.get(i).getText(),convertToDouble(price));
+                String price = productJson.get("price").toString();
+                String description = productJson.get("name").toString();
+                Items.put(description,convertToDouble(price));
             }
 
         } finally {
